3be88a89faf23e0d385031fd2f649e097257b149
Daiana Iurescu <daianaiurescu@daianas-air.home>
2020-05-08 12:08:13 +0300

    .

--- AntiqueStore/src/main/java/org/fis/student/Main.java
+++ AntiqueStore/src/main/java/org/fis/student/Main.java
@@ -20,4 +20,5 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
 }
--- AntiqueStore/src/main/java/org/fis/student/controllers/LogInController.java
+++ AntiqueStore/src/main/java/org/fis/student/controllers/LogInController.java
@@ -1,5 +1,6 @@
 package org.fis.student.controllers;
 
+import java.io.IOException;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,9 +9,13 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.Node;
+
+import static javafx.fxml.FXMLLoader.load;
+
 
 public class LogInController {
 
@@ -22,10 +27,12 @@ public class LogInController {
     public TextField mailField;
     @FXML
     public ChoiceBox roleField;
+    @FXML
+    private Button LogIn, Donate;
 
     @FXML
-    public void LogIn(ActionEvent Event) throws Exception{
-        String mail = mailField.getText();
+    public void LogIn(ActionEvent Event) throws Exception {
+       /** String mail = mailField.getText();
         String password = passwordField.getText();
         roleField.getItems().addAll("Client", "Administrator");
         if (mail == null || mail.isEmpty()) {
@@ -36,20 +43,45 @@ public class LogInController {
         if (password == null || password.isEmpty()) {
             loginMessage.setText("Password cannot be empty");
             return;
-        }
-        Stage stage=new Stage();
-        if(roleField.getValue().equals("Client")){
-            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("viewBooks.fxml"));
-            stage.setTitle("Log In");
-            stage.setScene(new Scene(root));
+        }**/
+        /**
+         if(roleField.getValue().equals("Client")){
+         Parent root = FXMLLoader.load(getClass().getResource("viewBooks.fxml"));
+         stage.setTitle("Log In");
+         stage.setScene(new Scene(root));
+         stage.show();
+         }
+         if(roleField.getValue().equals("Administrator")){
+         Parent root = FXMLLoader.load(getClass().getResource("AdminView.fxml"));
+         stage.setTitle("Log In");
+         stage.setScene(new Scene(root));
+         stage.show();
+         }**/
+    }
+
+    @FXML
+    public void Donate() {
+        /**try {
+            Parent donate = load(getClass().getResource("donateForm.fxml"));
+            Scene donateScene = new Scene(donate);
+            Stage window = (Stage) (Donate).getScene().getWindow();
+            window.setScene(donateScene);
+            window.show();
+        }catch(IOException e){
+            e.printStackTrace();
+        }**/
+
+        try {
+            Stage stage = (Stage) Donate.getScene().getWindow();
+            Parent donateFormRoot = load(getClass().getResource("donateForm.fxml"));
+            Scene scene = new Scene(donateFormRoot);
+            stage.setScene(scene);
             stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        if(roleField.getValue().equals("Administrator")){
-            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("AdminView.fxml"));
-            stage.setTitle("Log In");
-            stage.setScene(new Scene(root));
-            stage.show();
-        }
+
+
     }
 
 }
